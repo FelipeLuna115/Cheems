e12104ce2eb294bce4f578d00267262ee98e9fdd
Felipe Luna <68486389+FelipeLuna115@users.noreply.github.com>
2023-10-01 12:59:42 -0600

    index.js

--- js/index.js
+++ js/index.js
@@ -22,6 +22,11 @@ var clickable = false;
 //document.ondragstart = function(){return false}
 //document.onselectstart = function(){return false}
 //document.onmousedown = function() {return false}
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
 
 function RemoveSelectedCheems()
 {
